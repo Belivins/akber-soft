20d04885ea22b10f1f5fee56b094afb446a876b1
Belivins <59792105+Belivins@users.noreply.github.com>
2023-10-23 22:42:36 +0500

    Update server.erl

--- examples/server.erl
+++ examples/server.erl
@@ -2,12 +2,9 @@
 -export([main/0, send/2]).
 
 main() ->
-    %application:ensure_started(chumak),
     application:start(chumak),
     {ok, Socket} = chumak:socket(pub, "Dealer"),
     chumak:connect(Socket, tcp, "localhost", 5555),
-    
-    %{ok, _BindPid} = chumak:bind(Socket, tcp, "localhost", 5555),
     Socket.
 
 
