080202ce6f693e490046b8ed79ba94f948110997
Belivins <59792105+Belivins@users.noreply.github.com>
2023-10-23 22:44:10 +0500

    Update server.erl

--- examples/server.erl
+++ examples/server.erl
@@ -4,7 +4,15 @@
 main() ->
     application:start(chumak),
     {ok, Socket} = chumak:socket(pub, "Dealer"),
-    chumak:connect(Socket, tcp, "localhost", 5555),
+    % chumak:connect(Socket, tcp, "localhost", 5555),
+    case chumak:connect(Socket, tcp, "localhost", 5555) of
+        {ok, _BindPid} ->
+            io:format("Binding OK wiht Pid: ~p\n", [Socket]);
+        {error, Reason} ->
+            io:format("Connection Failed for this reason: ~p\n", [Reason]);
+        X ->
+            io:format("Unhandled reply for bind ~p \n", [X])
+    end,
     Socket.
 
 
